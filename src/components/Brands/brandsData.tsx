2c1fe0292a636da739cb9272105c582e6d0e5743
bacloud22 <yanna92yar@gmail.com>
2025-01-21 15:25:04 +0100

    nice

--- src/components/Brands/brandsData.tsx
+++ src/components/Brands/brandsData.tsx
@@ -2,46 +2,39 @@ import { Brand } from "@/types/brand";
 
 const brandsData: Brand[] = [
   {
-    id: 6,
-    name: "Formbold",
+    id: 1,
+    name: "NodeJS",
     href: "https://formbold.com",
     image: "/images/brands/formbold.svg",
     imageLight: "/images/brands/formbold-light.svg",
   },
   {
-    id: 1,
-    name: "UIdeck",
+    id: 2,
+    name: "MongoDB",
     href: "https://uideck.com",
     image: "/images/brands/uideck.svg",
     imageLight: "/images/brands/uideck-light.svg",
   },
   {
-    id: 2,
-    name: "Tailgrids",
+    id: 3,
+    name: "Redis",
     href: "https://tailgrids.com",
     image: "/images/brands/tailgrids.svg",
     imageLight: "/images/brands/tailgrids-light.svg",
   },
   {
-    id: 3,
-    name: "Lineicons",
+    id: 4,
+    name: "Docker",
     href: "https://lineicons.com",
     image: "/images/brands/lineicons.svg",
     imageLight: "/images/brands/lineicons-light.svg",
   },
   {
-    id: 4,
-    name: "Tailadmin",
+    id: 5,
+    name: "JavaScript",
     href: "https://tailadmin.com",
     image: "/images/brands/tailadmin.svg",
     imageLight: "/images/brands/tailadmin-light.svg",
-  },
-  {
-    id: 5,
-    name: "PlainAdmin",
-    href: "https://plainadmin.com",
-    image: "/images/brands/plainadmin.svg",
-    imageLight: "/images/brands/plainadmin-light.svg",
   },
 ];
 
