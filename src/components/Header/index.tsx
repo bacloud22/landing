79a310b4623f95b2c388a19da29538f95b705ead
bacloud22 <yanna92yar@gmail.com>
2025-01-21 15:25:04 +0100

    nice

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -57,14 +57,14 @@ const Header = () => {
                 } `}
               >
                 <Image
-                  src="/images/logo/logo-2.svg"
+                  src="/landing/images/logo/logo-2.svg"
                   alt="logo"
                   width={140}
                   height={30}
                   className="w-full dark:hidden"
                 />
                 <Image
-                  src="/images/logo/logo.svg"
+                  src="/landing/images/logo/logo.svg"
                   alt="logo"
                   width={140}
                   height={30}
