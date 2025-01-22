09cb2a2f4fcba7a68eb89dae98b840cbfebcdb81
open-listings <open-listings@gmx.com>
2025-01-22 21:44:29 +0100

    nice

--- src/components/Brands/index.tsx
+++ src/components/Brands/index.tsx
@@ -28,9 +28,9 @@ const SingleBrand = ({ brand }: { brand: Brand }) => {
   return (
     <div className="flex w-1/2 items-center justify-center px-3 py-[15px] sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6">
       <a
-        href={href}
-        target="_blank"
-        rel="nofollow noreferrer"
+        // href={href}
+        // target="_blank"
+        // rel="nofollow noreferrer"
         className="dark:opacity-60 dark:hover:opacity-100 relative h-10 w-full opacity-70 transition hover:opacity-100"
       >
         {/* <Image src={imageLight} alt={name} fill className="hidden dark:block" /> */}
