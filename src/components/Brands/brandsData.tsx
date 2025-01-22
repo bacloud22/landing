caa8e57d439b1a9646181b8b5ad6c5aae8514fb3
open-listings <open-listings@gmx.com>
2025-01-22 08:54:56 +0100

    Initial commit

---
+++ src/components/Brands/brandsData.tsx
@@ -0,0 +1,41 @@
+import { Brand } from "@/types/brand";
+
+const brandsData: Brand[] = [
+  {
+    id: 1,
+    name: "NodeJS",
+    href: "https://formbold.com",
+    image: "/images/brands/node.svg",
+    imageLight: "/images/brands/node.svg",
+  },
+  {
+    id: 2,
+    name: "MongoDB",
+    href: "https://uideck.com",
+    image: "/images/brands/mongodb.svg",
+    imageLight: "/images/brands/mongodb.svg",
+  },
+  {
+    id: 3,
+    name: "Redis",
+    href: "https://tailgrids.com",
+    image: "/images/brands/redis.svg",
+    imageLight: "/images/brands/redis.svg",
+  },
+  {
+    id: 4,
+    name: "Docker",
+    href: "https://lineicons.com",
+    image: "/images/brands/docker.svg",
+    imageLight: "/images/brands/docker.svg",
+  },
+  {
+    id: 5,
+    name: "JavaScript",
+    href: "https://tailadmin.com",
+    image: "/images/brands/javascript.svg",
+    imageLight: "/images/brands/javascript.svg",
+  },
+];
+
+export default brandsData;
---
+++ src/components/Features/index.tsx
@@ -0,0 +1,27 @@
+import SectionTitle from "../Common/SectionTitle";
+import SingleFeature from "./SingleFeature";
+import featuresData from "./featuresData";
+
+const Features = () => {
+  return (
+    <>
+      <section id="features" className="bg-white py-16 md:py-20 lg:py-28">
+        <div className="container">
+          <SectionTitle
+            title="Main Features"
+            paragraph="Open-listings is minimal and performant; It is a modern version of Cregslist like classifieds."
+            center
+          />
+
+          <div className="grid grid-cols-1 gap-x-8 gap-y-14 md:grid-cols-2 lg:grid-cols-3">
+            {featuresData.map((feature) => (
+              <SingleFeature key={feature.id} feature={feature} />
+            ))}
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Features;
