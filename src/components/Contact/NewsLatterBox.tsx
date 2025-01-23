697761e27ce11252dd5757322a77eb4b734a9ed2
bacloud22 <yanna92yar@gmail.com>
2025-01-23 15:02:32 +0100

    nice

--- src/components/Contact/NewsLatterBox.tsx
+++ src/components/Contact/NewsLatterBox.tsx
@@ -1,41 +1,85 @@
 "use client";
 
 import { useTheme } from "next-themes";
+import { useState } from "react";
 
 const NewsLatterBox = () => {
   const { theme } = useTheme();
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [firstname, setFirstname] = useState("");
+  const [email, setEmail] = useState("");
+
+  const handleSubscribe = async () => {
+    try {
+      const url = "https://api.sender.net/v2/subscribers";
+      const apiKey = "YOUR_API_KEY"; // Replace with your API key
+
+      const data = {
+        email: email,
+        firstname: firstname,
+        groups: ["group_id"], // Replace with your group ID(s)
+        trigger_automation: false,
+      };
+
+      const headers = {
+        Authorization: `Bearer ${apiKey}`,
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      };
+
+      const response = await fetch(url, {
+        method: "POST",
+        headers,
+        body: JSON.stringify(data),
+      });
+
+      if (response.status === 200) {
+        alert("You have successfully subscribed to my newsletter.");
+      } else {
+        alert("Failed to subscribe. Please try again.");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    setIsLoading(true);
+    handleSubscribe();
+  };
 
   return (
     <div className="relative z-10 rounded-sm bg-white p-8 shadow-three dark:bg-gray-dark sm:p-11 lg:p-8 xl:p-11">
       <h3 className="mb-4 text-2xl font-bold leading-tight text-black dark:text-white">
         Subscribe to receive future updates
       </h3>
-      <p className="mb-11 border-b border-body-color border-opacity-25 pb-11 text-base leading-relaxed text-body-color dark:border-white dark:border-opacity-25">
-        Lorem ipsum dolor sited Sed ullam corper consectur adipiscing Mae ornare
-        massa quis lectus.
+      <p className="mb-11 text-base leading-relaxed text-body-color dark:border-white dark:border-opacity-25">
+        No spam guaranteed, So please don’t send any spam mail.
       </p>
-      <div>
+      <form onSubmit={handleSubmit}>
         <input
           type="text"
-          name="name"
-          placeholder="Enter your name"
+          value={firstname}
+          onChange={e => setFirstname(e.target.value)}
+          placeholder="Your first name"
           className="border-stroke mb-4 w-full rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:text-body-color-dark dark:shadow-two dark:focus:border-primary dark:focus:shadow-none"
         />
         <input
           type="email"
-          name="email"
+          value={email}
+          onChange={e => setEmail(e.target.value)}
           placeholder="Enter your email"
           className="border-stroke mb-4 w-full rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:text-body-color-dark dark:shadow-two dark:focus:border-primary dark:focus:shadow-none"
         />
-        <input
-          type="submit"
-          value="Subscribe"
-          className="mb-5 flex w-full cursor-pointer items-center justify-center rounded-sm bg-primary px-9 py-4 text-base font-medium text-white shadow-submit duration-300 hover:bg-primary/90 dark:shadow-submit-dark"
-        />
-        <p className="text-center text-base leading-relaxed text-body-color dark:text-body-color-dark">
-          No spam guaranteed, So please don’t send any spam mail.
-        </p>
-      </div>
+        <button type="submit" disabled={isLoading} className="flex w-full items-center justify-center rounded-sm bg-primary p-3 text-base font-semibold text-white transition duration-300 ease-in-out hover:bg-opacity-80 hover:shadow-signUp">
+          {isLoading ? "Subscribing..." : "Subscribe"}
+        </button>
+      </form>
 
       <div>
         <span className="absolute left-2 top-7">
