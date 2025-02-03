d7fe73a4e12e9cbc7783c542383ff2b9e3af5a43
open-listings <open-listings@gmx.com>
2025-02-03 20:42:25 +0100

    your new commit message

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -32,7 +32,7 @@ const Hero = () => {
                     🔥 Get a license
                   </button>
                   <Link
-                    href="https://github.com/bacloud22/TODO:"
+                    href="https://calendly.com/sracer2016"
                     className="inline-block rounded-sm bg-black px-8 py-4 text-base font-semibold text-white duration-300 ease-in-out hover:bg-black/90 dark:bg-white/10 dark:text-white dark:hover:bg-white/5"
                   >
                     Schedule a meeting
